6f4fef15bdbb1779b5f629e0524daa2f5dcfa7f1
JavierCarpintero <100788494+JavierCarpintero@users.noreply.github.com>
2024-09-22 11:34:01 +0200

    First Commit

---
+++ javascript/elementsModule.js
@@ -0,0 +1,50 @@
+let link = document.createElement('link');
+link.rel = 'stylesheet';
+link.type = 'text/css';
+link.href = './style/style_01.css';
+
+function edit(){
+    window.alert("hola");
+}
+
+function addBasic(text) {
+    // create a new div element
+    const newDiv = document.createElement("div"); 
+
+    // and give it some content
+    const newContent = document.createTextNode(text);
+
+    // add the text node to the newly created div
+    newDiv.appendChild(newContent); 
+    newDiv.appendChild(link); 
+    
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("basic");
+    document.body.insertBefore(newDiv, currentDiv);
+}
+
+function addRow(row) {
+    const button = document.createElement("button");
+    button.appendChild(document.createTextNode("edit product")); 
+    button.appendChild(link); 
+    button.classList.add("editButton");
+    button.onclick = edit;
+    const newDiv = document.createElement("div"); 
+    newDiv.appendChild(link); 
+    newDiv.classList.add("row")
+
+    for(let i=0; i<row.length; i++){
+        const newColumn = document.createElement("div");
+        const newContent = document.createTextNode(row[i]);
+        newColumn.appendChild(link);
+        newColumn.classList.add("column")
+        newColumn.appendChild(newContent);
+        newDiv.appendChild(newColumn);
+    }
+
+    newDiv.appendChild(button);
+    const currentDiv = document.getElementById("final");
+    document.body.insertBefore(newDiv, currentDiv);
+}
+
+export { addBasic, addRow }
